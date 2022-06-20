b6de0911132b7a75d95659525a536245d2a2b3b0
gomesluiz <gomes.luiz@gmail.com>
2022-06-20 09:14:32 -0300

    task #3: calcula análise de similaridade entre sellers e buyers

---
+++ src/sellers_buyes_similarity.py
@@ -0,0 +1,22 @@
+"""Calculate similaries between sellers and buyers."""
+
+# Import third-part packages.
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+# Read the sellers and buyers data
+sellers_corpus = pd.Series({'1':'2 a 15'})
+buyers_corpus  = pd.Series({'1':'2 a 10'})
+
+# Create TfidfVectorizer object
+vectorizer = TfidfVectorizer()
+
+# Generate matrix of word vectors.
+sellers_matrix = vectorizer.fit_transform(sellers_corpus)
+buyers_matrix = vectorizer.transform(buyers_corpus)
+similarity = cosine_similarity(sellers_matrix, buyers_matrix)
+
+# Print the shape of matrices
+print(f"Sellers matrix: {sellers_matrix.shape}")
+print(f"Buyers  matrix: {buyers_matrix.shape}")
+print(f"Sellers and buyers  similarity: {similarity}")
