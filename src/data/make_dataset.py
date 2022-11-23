a04de011f435956ed7648636b7f146b1760ecb68
Luiz Gomes <gomes.luiz@gmail.com>
2022-11-22 23:47:48 -0300

    Add BERTimbau tokenizer

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -26,10 +26,6 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """ 
-    for file in files:
-        
-
-
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
